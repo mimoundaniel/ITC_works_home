39cab0a7aa2197aae11d83bfb7945b80d895fd74
danoux0613 <danieldmy13@gmail.com>
2022-07-05 00:12:36 +0300

    v1.4.0

--- js/about.js
+++ js/about.js
@@ -1,26 +1,7 @@
-function setButtonDisplay() {
-    var hidePrev = $('#citys div:first').hasClass('active'),
-        hideNext = $('#citys div:last').hasClass('active');
-
-    $('#back')[hidePrev ? 'hide' : 'show']();
-    $('#next')[hideNext ? 'hide' : 'show']();
+function success() {
+    if (document.getElementById("textsend").value === "" || document.getElementById("textsend2").value === "" || document.getElementById("textsend3").value === "") {
+        document.getElementById('button').disabled = true;
+    } else {
+        document.getElementById('button').disabled = false;
+    }
 }
-setButtonDisplay();
-
-$('#next').click(function() {
-    var $current = $('.city.active');
-    if ($(".city").next().length != 0) {
-        $('.city').removeClass('active');
-        $current.next().addClass('active');
-    }
-    setButtonDisplay();
-});
-
-$('#back').click(function() {
-    var $current = $('.city.active');
-    if ($(".city").prev().length != 0) {
-        $('.city').removeClass('active');
-        $current.prev().addClass('active');
-    }
-    setButtonDisplay();
-});
