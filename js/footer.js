2c9155b784ef1fe37d8d85a5700660e966835b26
danoux0613 <danieldmy13@gmail.com>
2022-07-04 21:30:13 +0300

    V.1

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,9 @@
 const codingLanguages = ["HTML", "CSS", "JavaScript"];
-codingLanguages.splice(2, 0, " and ");
-document.getElementById("table").innerHTML = codingLanguages;
+codingLanguages.splice(1, 0, ",  ");
+codingLanguages.splice(3, 0, " and ");
+
+
+document.getElementById("table").innerHTML = codingLanguages.join(" ");
+
+
+// document.getElementById("table").innerHTML = codingLanguages;
